d5272442d66ad1bdf127d676ebb86fe1999417a1
Brocel <geenens.prof@gmail.com>
2021-06-29 17:25:59 -0300

    First Commit

---
+++ src/main/java/com/brocels/springboot/backend/EnjeuSpringBootRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.brocels.springboot.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnjeuSpringBootRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnjeuSpringBootRestApiApplication.class, args);
+	}
+
+}
