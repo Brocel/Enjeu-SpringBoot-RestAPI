ea204e7dbe6081abd28aa976efbd4cd4a64f5c89
Brocel <geenens.prof@gmail.com>
2021-06-30 09:59:14 -0300

    adding constructor / getter & setters

--- src/main/java/com/brocels/springboot/backend/model/User.java
+++ src/main/java/com/brocels/springboot/backend/model/User.java
@@ -62,5 +62,115 @@ public class User {
 	private Set<Role> roles = new HashSet<>();
     
 
+	public User() {
+		
+	}
+
+	public User(@Size(max = 20) String firstName, @Size(max = 20) String lastName,
+			@Size(max = 20) @NotBlank String userName,
+			@NotBlank @Size(max = 50) @Email(message = "Email should be valid") String email,
+			@Min(value = 10, message = "Age shouldn't be under 10") int age, String country, String state, String city,
+			@NotBlank @Size(max = 120) String password) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.userName = userName;
+		this.email = email;
+		this.age = age;
+		this.country = country;
+		this.state = state;
+		this.city = city;
+		this.password = password;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+	
+	
 	
 }
