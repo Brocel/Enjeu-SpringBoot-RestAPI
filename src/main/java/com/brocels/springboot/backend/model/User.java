e2f01cd9c8e5fec548182f2067b38361f2357342
Brocel <geenens.prof@gmail.com>
2021-06-30 09:51:42 -0300

    add Erole , Role + def user entity

--- src/main/java/com/brocels/springboot/backend/model/User.java
+++ src/main/java/com/brocels/springboot/backend/model/User.java
@@ -60,6 +60,7 @@ public class User {
 				joinColumns = @JoinColumn(name = "user_id"), 
 				inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles = new HashSet<>();
+    
 
 	
 }
